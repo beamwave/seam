9cc2df2fb01ee43ca2f7b246f701c002527971eb
Eric Morrison <malin1@protonmail.ch>
2018-04-07 13:31:55 -0400

    fixed sidebar

--- client/app/routers/AppRouters.jsx
+++ client/app/routers/AppRouters.jsx
@@ -5,13 +5,10 @@ import createHistory from 'history/createBrowserHistory'
 import { hot } from 'react-hot-loader'
 import { StripeProvider } from 'react-stripe-elements'
 import decode from 'jwt-decode'
-import Sidebar from 'react-sidebar'
 
 import PrivateRoute from './PrivateRoute.jsx'
 import PublicRoute from './PublicRoute.jsx'
 
-import SidebarContent from '../components/SidebarContent.jsx'
-import ModalContainer from '../components/ModalContainer.jsx'
 import SplashPage from '../components/SplashPage.jsx'
 import LoginPage from '../components/LoginPage.jsx'
 import SignupPage from '../components/SignupPage.jsx'
@@ -24,7 +21,6 @@ import LoadingPage from '../components/LoadingPage.jsx'
 import NotFoundPage from '../components/NotFoundPage.jsx'
 
 import { login, startDisplay } from '../actions/auth'
-import { toggleSidebar } from '../actions/app'
 import configureStore from '../store/configureStore'
 
 // alternative to router
@@ -43,57 +39,22 @@ if (localStorage.appJWT) {
   store.dispatch(login(user))
 }
 
-const mql = window.matchMedia(`(min-width: 800px)`)
-
 export class AppRouter extends Component {
-  state = {
-    mql: mql,
-    docked: this.props.docked,
-    sidebarOpen: false
-  }
-
-  componentWillMount = () => {
-    mql.addListener(this.mediaQueryChanged)
-    this.setState({ mql: mql, sidebarDocked: mql.matches })
-  }
-
-  componentWillUnmount = () =>
-    this.state.mql.removeListener(this.mediaQueryChanged)
-
-  mediaQueryChanged = () =>
-    this.setState({ sidebarDocked: this.state.mql.matches })
-
-  onSetSidebarOpen = () =>
-    this.setState({ sidebarOpen: !this.state.sidebarOpen })
-
   render = () => {
-    const sidebar = <SidebarContent />
     return (
       <Provider store={store}>
         <StripeProvider apiKey="pk_test_XBcboz66T7bviKFmVNjtL7CK">
           <Router history={history}>
-            <Sidebar
-              sidebar={sidebar}
-              open={this.state.sidebarOpen}
-              docked={this.state.sidebarDocked}
-              onSetOpen={this.onSetSidebarOpen}
-            >
-              <Switch>
-                <PublicRoute exact path="/" component={SplashPage} />
-                <PublicRoute path="/login" component={LoginPage} />
-                <PublicRoute path="/signup" component={SignupPage} />
-                <PrivateRoute
-                  path="/dashboard"
-                  onSetSidebarOpen={this.onSetSidebarOpen}
-                  component={DashboardPage}
-                />
-                {/* <PrivateRoute path="/needs" component={NeedsPage} />
+            <Switch>
+              <PublicRoute exact path="/" component={SplashPage} />
+              <PublicRoute path="/login" component={LoginPage} />
+              <PublicRoute path="/signup" component={SignupPage} />
+              <PrivateRoute path="/dashboard" component={DashboardPage} />
+              {/* <PrivateRoute path="/needs" component={NeedsPage} />
                 <PrivateRoute path="/wants" component={WantsPage} />
                 <PrivateRoute path="/payments" component={PaymentPage} /> */}
-                <Route component={NotFoundPage} />
-              </Switch>
-              <ModalContainer />
-            </Sidebar>
+              <Route component={NotFoundPage} />
+            </Switch>
           </Router>
         </StripeProvider>
       </Provider>
--- client/app/routers/PrivateRoute.jsx
+++ client/app/routers/PrivateRoute.jsx
@@ -1,33 +1,97 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Route, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import Header from '../components/Header.jsx'
 
+import Sidebar from 'react-sidebar'
+import SidebarContent from '../components/SidebarContent.jsx'
+import ModalContainer from '../components/ModalContainer.jsx'
+import { toggleSidebar } from '../actions/app'
+
+const mql = window.matchMedia(`(min-width: 800px)`)
+
+class PrivateRoute extends Component {
+  state = {
+    mql: mql,
+    docked: this.props.docked,
+    sidebarOpen: false
+  }
+
+  componentWillMount = () => {
+    mql.addListener(this.mediaQueryChanged)
+    this.setState({ mql: mql, sidebarDocked: mql.matches })
+  }
+
+  componentWillUnmount = () =>
+    this.state.mql.removeListener(this.mediaQueryChanged)
+
+  mediaQueryChanged = () =>
+    this.setState({ sidebarDocked: this.state.mql.matches })
+
+  onSetSidebarOpen = () =>
+    this.setState({ sidebarOpen: !this.state.sidebarOpen })
+
+  render = () => {
+    const {
+      isAuthenticated,
+      onSetSidebarOpen,
+      component: Component,
+      ...rest
+    } = this.props
+
+    const sidebar = <SidebarContent />
+
+    return (
+      <Route
+        {...rest}
+        component={props =>
+          isAuthenticated ? (
+            <div>
+              <Sidebar
+                sidebar={sidebar}
+                open={this.state.sidebarOpen}
+                docked={this.state.sidebarDocked}
+                onSetOpen={this.onSetSidebarOpen}
+              >
+                <Header onSetSidebarOpen={this.onSetSidebarOpen} />
+                <Component {...props} />
+              </Sidebar>
+              <ModalContainer />
+            </div>
+          ) : (
+            <Redirect to="/login" />
+          )
+        }
+      />
+    )
+  }
+}
+
 // gets isAuthenticated from mapStateToProps
 // gets component from actual component in ./AppRouters.jsx
 // gets ...rest (route information) from Router in ./AppRouters.jsx
-const PrivateRoute = ({
-  isAuthenticated,
-  onSetSidebarOpen,
-  component: Component,
-  ...rest
-}) => {
-  return (
-    <Route
-      {...rest}
-      component={props =>
-        isAuthenticated ? (
-          <div>
-            <Header onSetSidebarOpen={onSetSidebarOpen} />
-            <Component {...props} />
-          </div>
-        ) : (
-          <Redirect to="/login" />
-        )
-      }
-    />
-  )
-}
+// const PrivateRoute = ({
+//   isAuthenticated,
+//   onSetSidebarOpen,
+//   component: Component,
+//   ...rest
+// }) => {
+//   return (
+//     <Route
+//       {...rest}
+//       component={props =>
+//         isAuthenticated ? (
+//           <div>
+//             <Header onSetSidebarOpen={onSetSidebarOpen} />
+//             <Component {...props} />
+//           </div>
+//         ) : (
+//           <Redirect to="/login" />
+//         )
+//       }
+//     />
+//   )
+// }
 
 const mapStateToProps = state => ({
   isAuthenticated: !!state.auth.token
